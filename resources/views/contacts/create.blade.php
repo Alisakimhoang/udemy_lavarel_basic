388e998af2c6f72c9eea4e8e2ae3483d12b848b9
Tran Kim Hoang <trankimhoang.sense@gmail.com>
2024-06-05 10:58:15 +0900

    first

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            新規作成
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="grid ">
+<div class="bg-purple-900 h-72 flex items-center justify-center">
+    <div class="flex flex-col max-w-4xl items-center justify-center">
+        <div class=" text-white text-4xl font-bold">Contact Us</div>
+        <div class="m-2 text-white ">Contact us by filling this contact form.</div>
+    </div>
+</div>
+<div class="bg-white h-auto flex justify-center">
+    <div class="h-auto bg-white -mt-20 w-[80%] md:w-1/2 text-stone-700 font-medium shadow-lg">
+        <div class="m-4 font-bold">Your E-mail Address </div> <input placeholder="admin@tailwind.org "
+            class="border-2 ml-4 border-gray-400 h-10 w-3/4 text-gray-400 rounded">
+        <div class="m-4 font-bold">Subject </div> <input
+            class="border-2 ml-4 border-gray-400 h-10 w-3/4 text-gray-400 rounded">
+        <div class="m-4 font-bold">How can we help? </div> <input
+            class="border-2 ml-4 border-gray-400 h-20 w-3/4 text-gray-400 rounded">
+        <div class="bg-purple-900 p-2 m-4 text-white w-max font-normal hover:bg-purple-700 cursor-pointer"> SEND
+        </div>
+    </div>
+</div>
+</div>
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> 
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('tests/test',[TestController::class,'index']);
+
+//Route::resource('contact',ContactFormController::class);
+
+Route::get('contacts',[ContactFormController::class,'index'])->name('contacts.index');
+
+Route::prefix('contacts')
+->middleware(['auth'])
+->controller(ContactFormController::class)
+->name('contacts.')
+->group(function(){
+ Route::get('/','index')->name('index');
+ Route::get('/create','create')->name('create');
+
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
